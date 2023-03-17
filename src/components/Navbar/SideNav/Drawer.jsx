f47fc37d10e71ac95c3ae322b6621f9e42ebbffd
Sarika.Sairam <sairamsarika24@gmail.com>
2023-03-17 16:12:40 +0530

    first commit

---
+++ src/components/Navbar/SideNav/Drawer.jsx
@@ -0,0 +1,35 @@
+import { styled, useTheme } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+const drawerWidth = 280;
+
+const StyledDrawer = styled(Drawer)(() => ({
+  backgroundColor: "#f1f1f1",
+  zIndex: 0,
+}));
+
+const MuiDrawer = ({ children, ...other }) => {
+  const theme = useTheme();
+
+  return (
+    <StyledDrawer
+      anchor='left'
+      sx={{
+        width: drawerWidth,
+        backgroundColor: "red",
+        ".css-dm4aar-MuiPaper-root-MuiDrawer-paper": { width: drawerWidth },
+
+        ".css-12i7wg6-MuiPaper-root-MuiDrawer-paper": {
+          width: drawerWidth,
+          borderRight: `2px solid ${theme.palette.primary.main}`,
+        },
+      }}
+      {...other}
+      elevation={5}
+      variant={"permanent"}
+    >
+      {children}
+    </StyledDrawer>
+  );
+};
+
+export default MuiDrawer;
